aabdb2d8126f62bf13095d2a78e41e0e90204358
Mahidhar <mahidharraju@gmail.com>
2020-12-07 11:33:35 +0530

    jacoco plugin added

--- src/main/java/com/org/trustservice/service/ITrustGroupService.java
+++ src/main/java/com/org/trustservice/service/ITrustGroupService.java
@@ -5,11 +5,26 @@ import com.org.trustservice.dto.TrustGroupUpdateResponseDTO;
 
 public interface ITrustGroupService {
 
+  /**
+   * get trustgroup details for a given department.
+   * @param orgCollabId
+   * @param departmentId
+   * @return TrustGroupUpdateResponseDTO.
+   */
   TrustGroupUpdateResponseDTO getAllTrustGroupsByDepartment(
       Long orgCollabId,
       Long departmentId);
 
+  /**
+   * updates trustgroup.
+   * @param requestDTO
+   */
   void updateTrustGroups(TrustGroupUpdateDTO requestDTO);
 
+  /**
+   * returns role assigned to a trust group.
+   * @param trustGroupId
+   * @return role value
+   */
   String getRoleBasedOnFlavour(Long trustGroupId);
 }
